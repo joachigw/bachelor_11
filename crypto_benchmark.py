a80de1aac9a76369bbad313369d05b7be50b255a
Joachim Grimen Westgaard <joachigw@stud.ntnu.no>
2025-05-05 18:48:52 +0200

    add separate method for timing function, refactor .env-read logic

--- crypto_benchmark.py
+++ crypto_benchmark.py
@@ -2,20 +2,29 @@ import os
 import shutil
 import pandas as pd
 from time import time
-from pathlib import Path
 from tabulate import tabulate
-from dotenv import load_dotenv
 from kdf_hmac_sha256 import derive_keys
 from encrypt_aesgcm import encrypt_files
+from utils_test_data import load_envs
+from typing import Callable, TypeVar, Any, Tuple
 
 
-load_dotenv()
-INPUT_DIR = os.environ.get("FILES_DIR")
-OUTPUT_DIR = os.environ.get("ENCRYPTED_DIR")
-CONFIG = os.environ.get("CONFIG")
-COMPANY_KEYS_PATH = os.environ.get("COMPANY_KEYS")
-Path(OUTPUT_DIR).mkdir(exist_ok=True)
+envs = load_envs()
 
+T = TypeVar("T")
+def time_function(function: Callable[..., T], *args: Any, **kwargs: Any) -> Tuple[T, float]:
+    """Measure elapsed runtime of a function.
+
+    :param function: the function to time, takes any arguments and returns any generic type
+    :param *args: list of arguments to pass to the function
+    :param **kargs: dict/keyword arguments to pass to the function
+    :return Tuple of result from function and elapsed time
+    """
+    start = time()
+    result = function(*args, **kwargs)
+    elapsed = time() - start
+    return result, elapsed
+    
 
 def run_benchmarks(n_rounds: int) -> tuple:
     """Run a specified number of rounds of deriving keys and encrypting files.
@@ -23,51 +32,50 @@ def run_benchmarks(n_rounds: int) -> tuple:
     :param n_rounds: number of rounds to run
     :return tuple of derivation time and rate, and encryption time and rate
     """
+    round_times = []
     derivation_times, derivation_rates = [], []
     encryption_times, encryption_rates = [], []
     for i in range(n_rounds):
         print(f"\n===ROUND {i+1}===")
         start_round = time()
 
-        start_derivation = time()
+        # Remove directory containing encrypted files, if present
+        if os.path.exists(envs["OUTPUT_DIR"]):
+            shutil.rmtree(envs["OUTPUT_DIR"])
+
         print(">Deriving keys...", end='')
-        keys = derive_keys(n_companies, n_departments, n_units, COMPANY_KEYS_PATH)
-        derivation_time = time() - start_derivation
-        derivation_times.append(f"{derivation_time:.5f}".replace('.', ','))
-        derivation_rates.append(f"{len(keys)/derivation_time:.0f}")
+        keys, time_derivation = time_function(derive_keys, n_companies, n_departments, n_units, envs["COMPANY_KEYS_PATH"])
+        derivation_times.append(f"{time_derivation:.5f}".replace('.', ','))
+        derivation_rates.append(f"{len(keys)//time_derivation}")
 
         print(">Encrypting files...", end='')
-        start_encryption = time()
-        encrypt_files(keys, INPUT_DIR, OUTPUT_DIR)
-        encryption_time = time() - start_encryption
-        encryption_times.append(f"{encryption_time:.5f}".replace('.', ','))
-        encryption_rates.append(f"{len(keys)/encryption_time:.0f}")
+        _, time_encryption = time_function(encrypt_files, keys, envs["INPUT_DIR"], envs["OUTPUT_DIR"])
+        encryption_times.append(f"{time_encryption:.5f}".replace('.', ','))
+        encryption_rates.append(f"{len(keys)//time_encryption}")
 
-        print(f"Total time elapsed: {(time()-start_round):.2f}s\n")
-    return derivation_times, derivation_rates, encryption_times, encryption_rates
+        round_times.append(f"{(time()-start_round):.5f}")
+
+    return round_times, derivation_times, derivation_rates, encryption_times, encryption_rates
 
 
 if __name__ == "__main__":
-    # Remove directory containing encrypted files, if present
-    if os.path.exists(OUTPUT_DIR):
-        shutil.rmtree(OUTPUT_DIR)
-    
-    # Read n_companies, n_departments and n_units
-    conf = pd.read_csv(CONFIG, header=None)[0].tolist()
+    # Read configured n_companies, n_departments and n_units
+    conf = pd.read_csv(envs["CONFIG"], header=None)[0].tolist()
     n_companies, n_departments, n_units = conf
     n_keys = n_companies*n_departments*n_units
     print(f"=== Parameters ===\n  keys to derive/files to encrypt: {n_keys:_}")
 
     # Time 5 rounds
-    derivation_times, derivation_rates, encryption_times, encryption_rates = run_benchmarks(5)
-    
+    round_times, derivation_times, derivation_rates, encryption_times, encryption_rates = run_benchmarks(5)
+
     # Print results in tabular format
-    headers = ["Round"] + [f"Round {i+1}" for i in range(5)]
+    headers = ["Round"] + [f"\033[1mRound {i+1}\033[0m" for i in range(5)]
     table = [
-        ["Key Derivation Time (s)"] + derivation_times,
-        ["Key Derivation Rate (keys/s)"] + derivation_rates,
-        ["File Encryption Time (s)"] + encryption_times,
-        ["File Encryption Rate (files/s)"] + encryption_rates,
+        ["Key Derivation (s)"] + derivation_times,
+        ["Key Derivation rate (keys/s)"] + derivation_rates,
+        ["File Encryption (s)"] + encryption_times,
+        ["File Encryption rate (files/s)"] + encryption_rates,
+        ["TOTAL (s)"] + round_times
     ]
     # Right-align all columns
     colalign = ["left"] + ["right"] * 5
