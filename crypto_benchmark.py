0a1065e124e76946985e51ea697591c1f80cdd77
Joachim Grimen Westgaard <joachigw@stud.ntnu.no>
2025-05-21 11:25:58 +0200

    add average col in results table

--- crypto_benchmark.py
+++ crypto_benchmark.py
@@ -45,15 +45,15 @@ def run_benchmarks(n_rounds: int) -> tuple:
 
         print(">Deriving keys...", end='')
         keys, time_derivation = time_function(derive_keys, n_companies, n_departments, n_units, envs["COMPANY_KEYS_PATH"])
-        derivation_times.append(f"{time_derivation:.5f}".replace('.', ','))
+        derivation_times.append(f"{time_derivation:.5f}")
         derivation_rates.append(f"{len(keys)//time_derivation}")
 
-        # print(">Encrypting files...", end='')
-        # _, time_encryption = time_function(encrypt_files, keys, envs["INPUT_DIR"], envs["OUTPUT_DIR"])
-        # encryption_times.append(f"{time_encryption:.5f}".replace('.', ','))
-        # encryption_rates.append(f"{len(keys)//time_encryption}")
+        print(">Encrypting files...", end='')
+        _, time_encryption = time_function(encrypt_files, keys, envs["INPUT_DIR"], envs["OUTPUT_DIR"])
+        encryption_times.append(f"{time_encryption:.5f}")
+        encryption_rates.append(f"{len(keys)//time_encryption}")
 
-        # round_times.append(f"{(time()-start_round):.5f}")
+        round_times.append(f"{(time()-start_round):.5f}")
 
     return round_times, derivation_times, derivation_rates, encryption_times, encryption_rates
 
@@ -69,17 +69,26 @@ if __name__ == "__main__":
     n_rounds = 5
     round_times, derivation_times, derivation_rates, encryption_times, encryption_rates = run_benchmarks(n_rounds)
 
+    # Calculate averages
+    average_der = [round(sum(list(map(float, derivation_times))) / n_rounds, 5)]
+    average_der_rate = [sum(list(map(float, derivation_rates))) // n_rounds]
+    average_enc = [round(sum(list(map(float, encryption_times))) / n_rounds, 5)]
+    average_enc_rate = [sum(list(map(float, encryption_rates))) // n_rounds]
+    average_tot = [round(sum(list(map(float, round_times))) / n_rounds, 5)]
+
     # Print results in tabular format
-    headers = ["Round"] + [f"\033[1mRound {i+1}\033[0m" for i in range(n_rounds)]
+    headers = ["Round"] + [f"\033[1mRound {i+1}\033[0m" for i in range(n_rounds)] + ["\033[1mAverage\033[0m"]
+    
     table = [
-        ["Key Derivation (s)"] + derivation_times,
-        ["Key Derivation rate (keys/s)"] + derivation_rates,
-        ["File Encryption (s)"] + encryption_times,
-        ["File Encryption rate (files/s)"] + encryption_rates,
-        ["TOTAL (s)"] + round_times
+        ["Key Derivation (s)"] + derivation_times + average_der,
+        ["Key Derivation rate (keys/s)"] + derivation_rates + average_der_rate,
+        ["File Encryption (s)"] + encryption_times + average_enc,
+        ["File Encryption rate (files/s)"] + encryption_rates + average_enc_rate,
+        ["TOTAL (s)"] + round_times + average_tot
     ]
+
     # Right-align all columns
-    colalign = ["left"] + ["right"] * 5
+    colalign = ["left"] + ["right"] * (n_rounds + 1)
 
     print(f"\n=== Results summary ({n_keys:_} keys derived and files encrypted) ===")
     print(tabulate(table, headers=headers, tablefmt="grid", colalign=colalign))
